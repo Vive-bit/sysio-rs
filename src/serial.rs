9afb1e87948e81dd760d56a7fec4e85a916fd73c
Martin Lechner <77213543+Vive-bit@users.noreply.github.com>
2025-08-06 00:27:34 +0200

    update config

--- src/serial.rs
+++ src/serial.rs
@@ -1,20 +1,75 @@
 use pyo3::prelude::*;
 use pyo3::exceptions::PyOSError;
+use pyo3::FromPyObject;
 use std::ffi::CString;
 use std::sync::Mutex;
-use libc::{ open, read, write, ioctl, tcgetattr, tcsetattr, cfmakeraw, cfsetispeed,
-            cfsetospeed, tcdrain, tcflush, termios, c_int, FIONREAD, TCIFLUSH,
-            TCOFLUSH, O_RDWR, O_NOCTTY, O_NONBLOCK, B9600, B38400, B115200,
-            TCSANOW, VMIN, VTIME, EAGAIN, EWOULDBLOCK };
+use libc::{
+    open, read, write, ioctl,
+    tcgetattr, tcsetattr, cfmakeraw, cfsetispeed, cfsetospeed,
+    tcdrain, tcflush, termios, c_int, FIONREAD, TCIFLUSH, TCOFLUSH,
+    O_RDWR, O_NOCTTY, O_NONBLOCK,
+    B9600, B38400, B115200,
+    TCSANOW, VMIN, VTIME, EAGAIN, EWOULDBLOCK,
+    CS5, CS6, CS7, CS8, PARENB, PARODD, CSTOPB,
+};
 use crate::gpio::{setup, output};
 
-fn config_serial(fd: c_int, baud: u32) -> Result<(), String> {
+#[derive(Debug, Clone, Copy, FromPyObject)]
+pub enum DataBits {
+    Five  = 5,
+    Six   = 6,
+    Seven = 7,
+    Eight = 8,
+}
+
+#[derive(Debug, Clone, Copy, FromPyObject)]
+pub enum Parity {
+    N, // None
+    E, // Even
+    O, // Odd
+}
+
+#[derive(Debug, Clone, Copy, FromPyObject)]
+pub enum StopBits {
+    One = 1,
+    Two = 2,
+}
+
+fn config_serial(
+    fd: c_int,
+    baud: u32,
+    timeout: f64,
+    data_bits: DataBits,
+    parity: Parity,
+    stop_bits: StopBits,
+) -> Result<(), String> {
     unsafe {
         let mut tio: termios = std::mem::zeroed();
         if tcgetattr(fd, &mut tio) != 0 {
             return Err("tcgetattr failed".into());
         }
         cfmakeraw(&mut tio);
+
+        tio.c_cflag &= !libc::CSIZE;
+        tio.c_cflag |= match data_bits {
+            DataBits::Five  => CS5,
+            DataBits::Six   => CS6,
+            DataBits::Seven => CS7,
+            DataBits::Eight => CS8,
+        };
+
+        match parity {
+            Parity::N => tio.c_cflag &= !PARENB,
+            Parity::E => { tio.c_cflag |= PARENB; tio.c_cflag &= !PARODD; },
+            Parity::O => { tio.c_cflag |= PARENB; tio.c_cflag |= PARODD; },
+        }
+
+        if let StopBits::Two = stop_bits {
+            tio.c_cflag |= CSTOPB;
+        } else {
+            tio.c_cflag &= !CSTOPB;
+        }
+
         let speed = match baud {
             9600   => B9600,
             38400  => B38400,
@@ -23,6 +78,10 @@ fn config_serial(fd: c_int, baud: u32) -> Result<(), String> {
         };
         cfsetispeed(&mut tio, speed);
         cfsetospeed(&mut tio, speed);
+
+        tio.c_cc[VMIN]  = 0;
+        tio.c_cc[VTIME] = (timeout * 10.0) as u8;
+
         if tcsetattr(fd, TCSANOW, &tio) != 0 {
             return Err("tcsetattr failed".into());
         }
@@ -39,22 +98,28 @@ pub struct Serial485 {
 #[pymethods]
 impl Serial485 {
     #[new]
-    pub fn new(path: String, baud: u32, timeout: f64, de_pin: u8) -> PyResult<Self> {
+    pub fn new(
+        path: String,
+        baud: u32,
+        timeout: f64,
+        de_pin: u8,
+        data_bits: DataBits,
+        parity: Parity,
+        stop_bits: StopBits,
+    ) -> PyResult<Self> {
         setup(de_pin, "OUT")?;
         output(de_pin, 0)?;
-        let cpath = CString::new(path).unwrap();
+
+        let cpath = CString::new(path)
+            .map_err(|e| PyOSError::new_err(e.to_string()))?;
         let raw_fd = unsafe { open(cpath.as_ptr(), O_RDWR | O_NOCTTY | O_NONBLOCK) };
         if raw_fd < 0 {
             return Err(PyOSError::new_err("Serial open failed"));
         }
-        config_serial(raw_fd, baud).map_err(PyOSError::new_err)?;
-        unsafe {
-            let mut tio: termios = std::mem::zeroed();
-            tcgetattr(raw_fd, &mut tio);
-            tio.c_cc[VMIN]  = 0;
-            tio.c_cc[VTIME] = (timeout * 10.0) as u8;
-            tcsetattr(raw_fd, TCSANOW, &tio);
-        }
+
+        config_serial(raw_fd, baud, timeout, data_bits, parity, stop_bits)
+            .map_err(PyOSError::new_err)?;
+
         Ok(Serial485 { fd: Mutex::new(raw_fd), de_pin })
     }
 
