a0b710d489196f7e40a00033ad98b679b937ae36
Martin Lechner <77213543+Vive-bit@users.noreply.github.com>
2025-08-05 13:36:36 +0200

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,21 @@
 use pyo3::prelude::*;
 use pyo3::exceptions::PyOSError;
-use std::fs::File;
-use std::os::unix::io::{AsRawFd, RawFd};
 use std::ptr;
-use libc::{open, mmap, munmap, close, O_RDWR, PROT_READ, PROT_WRITE, MAP_SHARED};
+use libc::{open, mmap, close, O_RDWR, PROT_READ, PROT_WRITE, MAP_SHARED};
 
-const GPIO_BASE_OFFSET: usize = 0x200000; // for Raspberry Pi 1; adjust for Pi2/3 (0x3F000000) or Pi4 (0xFE000000)
 const GPIO_LEN: usize = 0xB4;
 
 static mut GPIO_MEM: *mut u32 = ptr::null_mut();
 
-fn init_gpio() -> Result<(), PyOSError> {
+fn init_gpio() -> PyResult<()> {
     unsafe {
         if !GPIO_MEM.is_null() {
             return Ok(());
         }
-        // open /dev/gpiomem
-        let fd = open("/dev/gpiomem\0".as_ptr() as *const i8, O_RDWR);
+        let fd = open(b"/dev/gpiomem\0".as_ptr() as *const i8, O_RDWR);
         if fd < 0 {
             return Err(PyOSError::new_err("Failed to open /dev/gpiomem"));
         }
-        // mmap GPIO registers
         let map = mmap(
             ptr::null_mut(),
             GPIO_LEN,
@@ -29,12 +24,11 @@ fn init_gpio() -> Result<(), PyOSError> {
             fd,
             0,
         );
+        close(fd);
         if map == libc::MAP_FAILED {
-            close(fd);
             return Err(PyOSError::new_err("GPIO mmap failed"));
         }
         GPIO_MEM = map as *mut u32;
-        close(fd);
         Ok(())
     }
 }
@@ -45,8 +39,8 @@ fn setmode(_mode: &str) -> PyResult<()> {
     Ok(())
 }
 
-fn gpio_register(index: usize) -> *mut u32 {
-    unsafe { GPIO_MEM.add(index) }
+fn gpio_register(idx: usize) -> *mut u32 {
+    unsafe { GPIO_MEM.add(idx) }
 }
 
 #[pyfunction]
@@ -58,9 +52,8 @@ fn setup(pin: u8, direction: &str) -> PyResult<()> {
         let reg = gpio_register(fsel);
         let val = ptr::read_volatile(reg);
         let mask = !(0b111 << shift);
-        let new = (val & mask) |
-                  ((if direction == "OUT" { 0b001 } else { 0b000 }) << shift);
-        ptr::write_volatile(reg, new);
+        let mode_bits = if direction == "OUT" { 0b001 } else { 0b000 };
+        ptr::write_volatile(reg, (val & mask) | (mode_bits << shift));
     }
     Ok(())
 }
@@ -68,10 +61,10 @@ fn setup(pin: u8, direction: &str) -> PyResult<()> {
 #[pyfunction]
 fn output(pin: u8, value: u8) -> PyResult<()> {
     init_gpio()?;
-    let reg_index = if value == 0 { 10 + ((pin as usize) / 32) } else { 7 + ((pin as usize) / 32) };
+    let idx = if value == 0 { 10 + (pin as usize) / 32 } else { 7 + (pin as usize) / 32 };
     let shift = (pin % 32) as usize;
     unsafe {
-        let reg = gpio_register(reg_index);
+        let reg = gpio_register(idx);
         ptr::write_volatile(reg, 1 << shift);
     }
     Ok(())
@@ -80,10 +73,10 @@ fn output(pin: u8, value: u8) -> PyResult<()> {
 #[pyfunction]
 fn input(pin: u8) -> PyResult<u8> {
     init_gpio()?;
-    let reg = gpio_register(13 + ((pin as usize) / 32));
+    let idx = 13 + (pin as usize) / 32;
     let shift = (pin % 32) as usize;
     unsafe {
-        let val = ptr::read_volatile(reg);
+        let val = ptr::read_volatile(gpio_register(idx));
         Ok(((val >> shift) & 1) as u8)
     }
 }
