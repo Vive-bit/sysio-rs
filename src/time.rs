24f0a81d97386a78f66ea0d6fd9d804cf10b5d9c
Vive-bit <martinlechner210@gmail.com>
2025-08-05 20:42:55 +0200

    update

--- src/time.rs
+++ src/time.rs
@@ -1,13 +1,31 @@
 use pyo3::prelude::*;
 use std::thread::sleep;
 use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[pyfunction]
-pub fn sleep_ms(ms: u64) {
-    sleep(Duration::from_millis(ms));
+/// Sleeps for given seconds
+pub fn sleep_s(s: f64) {
+    sleep(Duration::from_secs_f64(s));
 }
 
 #[pyfunction]
-pub fn sleep_us(us: u64) {
-    sleep(Duration::from_micros(us));
+/// Sleeps for given milliseconds
+pub fn sleep_ms(ms: f64) {
+    sleep(Duration::from_secs_f64(ms / 1_000.0));
+}
+
+#[pyfunction]
+/// Sleeps for given microseconds
+pub fn sleep_us(us: f64) {
+    sleep(Duration::from_secs_f64(us / 1_000_000.0));
+}
+
+#[pyfunction]
+/// Return current time as float seconds since UNIX epoch
+pub fn time_time() -> PyResult<f64> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map_err(|e| PyOSError::new_err(format!("SystemTime error: {}", e)))?;
+    Ok(now.as_secs_f64())
 }
